8c446170d8dbe5a3197b813920b3be629ff4d234
Sander Rozemuller <s.rozemuller@exitecloud.com>
2023-04-11 12:15:31 +0200

    added fa config

---
+++ FunctionApps/MicrosoftEndpointManager/profile.ps1
@@ -0,0 +1,2 @@
+# You can define helper functions, run commands, or specify environment variables
+# any variables defined that are not environment variables will get reset after the first execution
---
+++ FunctionApps/MicrosoftEndpointManager/requirements.psd1
@@ -0,0 +1,5 @@
+@{
+    'Az.ResourceGraph' = '0.*'
+    'Az.Accounts'      = '2.*'
+    'Az.KeyVault'      = '4.*'
+}
