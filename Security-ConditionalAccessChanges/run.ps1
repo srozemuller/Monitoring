bef3d53e31698d5c3770360bed33cadb20c5f456
srozemuller <s.rozemuller@exite.com>
2023-06-07 22:32:19 +0200

    initial

---
+++ Security-ConditionalAccessChanges/run.ps1
@@ -0,0 +1,12 @@
+# Input bindings are passed in via param block.
+param($Request)
+
+# Get the current universal time in the default string format.
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+
+$Request
