dcfba7bbe43603bb76af506e4dc558f0472f9e06
Sander Rozemuller <43162899+srozemuller@users.noreply.github.com>
2024-05-30 13:17:24 +0200

    Update run.ps1

--- Security-BreakGlassCertRenewer/run.ps1
+++ Security-BreakGlassCertRenewer/run.ps1
@@ -63,6 +63,7 @@ try {
     $x509Cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList ($certBytes, $certPassword, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
     # Export the certificate with the private key
     $pfxBytes = $x509Cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pfx, $certPassword)
+    $certFilePath = "./"
     # Write the byte array to a file
     [System.IO.File]::WriteAllBytes($certFilePath, $pfxBytes)
     # Read the certificate file
